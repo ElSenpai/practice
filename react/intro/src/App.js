f1e9d9bd3ca431a569e0439fc294088219890281
El Senpai <El Senpai@EL-SENPAI>
2021-05-10 16:03:50 +0300

    hey

--- react/intro/src/App.js
+++ react/intro/src/App.js
@@ -4,66 +4,76 @@ import Navi from "./Navi";
 import ProductList from "./ProductList";
 import { Container, Row, Col } from 'reactstrap';
 import { Component } from "react";
+import alertify from "alertifyjs";
+import { Route, Switch } from "react-router";
+import NotFound from "./NotFound";
+import CartList from "./CartList";
 
-export default class App extends Component{
-  state={currentCategory:"",products:[],cart:[]}
-  changeCategory=(category)=>{
-    this.setState({currentCategory:category.categoryName});
+export default class App extends Component {
+  state = { currentCategory: "", products: [], cart: [] }
+  changeCategory = (category) => {
+    this.setState({ currentCategory: category.categoryName });
     this.getProducts(category.id);
- }
-
- componentDidMount(){
-   this.getProducts();
- }
- addToCart=(product)=>{
-  let newCart=this.state.cart;
-  var addedItem= newCart.find(c=>c.product.id===product.id);
-  if(addedItem){
-    addedItem.quantity+=1;
-
-  }else{
-     newCart.push({product:product,quantity:1});
   }
- 
-  this.setState({cart:newCart});
-}
-removeFromCart=(product)=>{
-  let newCart=this.state.cart.filter(c=>c.product.id!==product.id);
-  this.setState({cart:newCart})
-}
 
- getProducts=categoryId=>{
-   let url="http://localhost:3000/products";
-   if(categoryId){
-    url+="?categoryId="+categoryId;
-   }
-  fetch(url)
-  .then(res=>res.json())
-  .then(data=>this.setState({products:data}));
-}
- 
-  render(){
-     let productInfo={title:"Product List",baskabisey:"bişey"}
-   let categoryInfo={title:"Category List"}
-    return(
+  componentDidMount() {
+    this.getProducts();
+  }
+  addToCart = (product) => {
+    let newCart = this.state.cart;
+    var addedItem = newCart.find(c => c.product.id === product.id);
+    if (addedItem) {
+      addedItem.quantity += 1;
+
+    } else {
+      newCart.push({ product: product, quantity: 1 });
+    }
+
+    this.setState({ cart: newCart });
+    alertify.success(product.productName + "  Added to cart", 2)
+  }
+  removeFromCart = (product) => {
+    let newCart = this.state.cart.filter(c => c.product.id !== product.id);
+    this.setState({ cart: newCart })
+  }
+
+  getProducts = categoryId => {
+    let url = "http://localhost:3000/products";
+    if (categoryId) {
+      url += "?categoryId=" + categoryId;
+    }
+    fetch(url)
+      .then(res => res.json())
+      .then(data => this.setState({ products: data }));
+  }
+
+  render() {
+    let productInfo = { title: "Product List", baskabisey: "bişey" }
+    let categoryInfo = { title: "Category List" }
+    return (
       <div >
         <Container>
-           <Navi removeFromCart={this.removeFromCart} cart={this.state.cart}/>
-          
+          <Navi removeFromCart={this.removeFromCart} cart={this.state.cart} />
+
           <Row >
             <Col xs="4">
-              <CategoryList 
-               currentCategory={this.state.currentCategory}
+              <CategoryList
+                currentCategory={this.state.currentCategory}
                 changeCategory={this.changeCategory} info={categoryInfo}
-                 />
+              />
             </Col><Col xs="8">
-             <ProductList addToCart={this.addToCart}
-              products={this.state.products} currentCategory={this.state.currentCategory} info={productInfo} />
+              <Switch>
+              <Route  path="/"  />
+               <Route path="/cart" component={CartList} />
+                <Route exact path="/" component={NotFound} />
+              </Switch>
+              <ProductList addToCart={this.addToCart}
+                products={this.state.products} currentCategory={this.state.currentCategory} info={productInfo} />
             </Col>
-            </Row>
+          </Row>
         </Container>
       </div>
-  
+
     );
   }
 }
--- react/intro/src/index.js
+++ react/intro/src/index.js
@@ -4,14 +4,17 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'alertifyjs/build/css/alertify.min.css';
+import {BrowserRouter} from 'react-router-dom';
 
 
 ReactDOM.render(
-  <React.Fragment>
-    <App />
-  </React.Fragment>,
+  
+   <BrowserRouter> <App /></BrowserRouter>,
+  
   document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
